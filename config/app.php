054d06282e84ea24997b79a923e30b362a894c2e
sayo.arakawa <arakawa@consulting-r.biz>
2017-09-28 10:46:51 +0000

    user pages

--- config/app.php
+++ config/app.php
@@ -146,6 +146,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         
         Collective\Html\HtmlServiceProvider::class,
+        
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
     ],
 
     /*
@@ -196,7 +198,8 @@ return [
         
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
     ],
 
 ];
